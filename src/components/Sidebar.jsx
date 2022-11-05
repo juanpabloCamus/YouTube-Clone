de56f2865d900f6d38f16549a0b0bb4303fc6edd
Juan Pablo <juanpablocamus28@gmail.com>
2022-11-05 16:10:56 -0300

    add fetch function, selected category state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import { Stack } from "@mui/material"
 
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction='row'
     sx={{ overflowY: 'auto', height: { sx: 'auto', md:'95%' }, flexDirection: { md: 'column' }}}
@@ -12,6 +10,7 @@ const Sidebar = () => (
     {categories.map((cat) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(cat.name)}
         style={{background:  cat.name === selectedCategory && '#FC1503', color: 'white'}}
         key={cat.name}
       >
